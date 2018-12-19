e99d8502aa50563218ce35f7037c5504bd8cd7ac
Cyber <Cyber@172.40.73.51>
2018-12-19 22:49:46 +0900

    3 days. adding buff

--- pocket/src/pocket/n001_Bulbasaur.java
+++ pocket/src/pocket/n001_Bulbasaur.java
@@ -3,6 +3,7 @@ package pocket;
 public class n001_Bulbasaur extends pocket {
 	int _number = 1;
 	double _speed = 0.7, _ad = 0.7, _ap = 0.7, _adD = 0.7, _apD = 0.7, _HP = 0.7;
+	//int _number, double _speed, double _ad, double _ap, double _adD, double _apD, double _HP, Type[] _type
 	Type[] _type = {Type.GRASS, Type.POISON};
 	
 	public n001_Bulbasaur(String _name, int _level, int _iv) {
--- pocket/src/pocket/pocket.java
+++ pocket/src/pocket/pocket.java
@@ -31,6 +31,10 @@ public class pocket {
 		ELECTRICITY, FIGHTHING, FIRE, FLYING, GHOST, GRASS, GROUND, ICE, NORMAL, POISON, ROCK, WATER, NTH
 	}
 	
+	enum States{
+		BURN, PARALYSIS, ADDICTION, FROZEN, SLEEPING, REACTION, NTH
+	}
+	
 	//constant number
 	protected int number;
 	protected double const_speed, const_ad, const_ap, const_adD, const_apD, const_HP;// const_MP;
@@ -39,19 +43,80 @@ public class pocket {
 	protected String name;
 	protected int level;
 	protected double iv;
+	private States state;
 	//buff stack 
-	private int b_speed, d_speed, b_ad, d_ad, b_ap, d_ap, b_adD, d_adD, b_apD, d_apD;
+	private double accuracy;
+	private int b_speed, b_ad, b_ap, b_adD, b_apD, b_accu;//d_speed, d_ad, d_ap, d_adD, d_apD, d_accu
 	private int HP, maxHP;//MP, maxMP 
-	private int status;
 	
 	protected pocket(int _number, double _speed, double _ad, double _ap, double _adD, double _apD, double _HP, Type[] _type) {
 		this.number = _number; this.const_speed = _speed; this.const_ad = _ad; this.const_adD = _adD; this.const_ap = _ap; 
-		this.const_apD = _apD; this.const_HP = _HP; this.type = _type; //this.const_MP = _MP;
+		this.const_apD = _apD; this.const_HP = _HP; this.type = _type; this.state = State.NTH;//this.const_MP = _MP;
 	}
 	
-	protected setting()	{
-		this.b_speed = 0; this.b_ad = 0; this.b_adD = 0; this.b_ap = 0; this.b_apD = 0;
-		this.d_speed = 0; this.d_ad = 0; this.d_adD = 0; this.d_ap = 0; this.d_apD = 0;
+	public void setting() {
+		this.accuracy = 1;
+		this.b_speed = 0; this.b_ad = 0; this.b_adD = 0; this.b_ap = 0; this.b_apD = 0; this.b_accu;
+		//this.d_speed = 0; this.d_ad = 0; this.d_adD = 0; this.d_ap = 0; this.d_apD = 0; this.d_accu;
+	}
+	
+	//buff or debuff function
+	public boolean buff_speed(int n) {
+		if (this.b_speed < -4 || this.b_speed > 4) return false; //do not exist increasing or decreasing
+		this.b_speed += n;
+		if (this.isUpper(this.b_speed)) this.b_speed = 4;
+		else if (this.isLower(this.b_speed)) this.b_speed = -4;
+		return true;		//complete buffing or debuffing
+	}
+	
+	public boolean buff_ad(int n) {
+		if (this.b_ad < -4 || this.b_ad > 4) return false; //do not exist increasing or decreasing
+		this.b_ad += n;
+		if (this.isUpper(this.b_ad)) this.b_ad = 4;
+		else if (this.isLower(this.b_ad)) this.b_ad = -4;
+		return true;		//complete buffing or debuffing
+	}
+	
+	public boolean buff_adD(int n) {
+		if (this.b_adD < -4 || this.b_adD > 4) return false; //do not exist increasing or decreasing
+		this.b_adD += n;
+		if (this.isUpper(this.b_adD)) this.b_adD = 4;
+		else if (this.isLower(this.b_adD)) this.b_adD = -4;
+		return true;		//complete buffing or debuffing
+	}
+
+	public boolean buff_ap(int n) {
+		if (this.b_ap < -4 || this.b_ap > 4) return false; //do not exist increasing or decreasing
+		this.b_ap += n;
+		if (this.isUpper(this.b_ap)) this.b_ap = 4;
+		else if (this.isLower(this.b_ap)) this.b_ap = -4;
+		return true;		//complete buffing or debuffing
+	}
+
+	public boolean buff_apD(int n) {
+		if (this.b_apD < -4 || this.b_apD > 4) return false; //do not exist increasing or decreasing
+		this.b_apD += n;
+		if (this.isUpper(this.b_apD)) this.b_apD = 4;
+		else if (this.isLower(this.b_apD)) this.b_apD = -4;
+		return true;		//complete buffing or debuffing
+	}
+	
+	public boolean buff_accu(int n) {
+		if (this.b_accu < -4 || this.b_accu > 4) return false; //do not exist increasing or decreasing
+		this.b_accu += n;
+		if (this.isUpper(this.b_accu)) this.b_accu = 4;
+		else if (this.isLower(this.b_accu)) this.b_accu = -4;
+		return true;		//complete buffing or debuffing
+	}
+	//development tools
+	protected boolean isUpper(int n) {
+		if (n > 4) return true;
+		return false;
+	}
+	
+	protected boolean isLower(int n) {
+		if (n < -4) return true;
+		return false;
 	}
 }
 
