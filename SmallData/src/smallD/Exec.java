7aed3470fb1dae7776e30d09e18269032239099f
Cyber <Cyber@172.40.73.81>
2020-04-05 14:49:36 +0900

    day 3

--- SmallData/src/smallD/Exec.java
+++ SmallData/src/smallD/Exec.java
@@ -61,14 +61,23 @@ public class Exec {
 	}
 	
 	public void writeExel() throws IOException {
-		FileInputStream cellData = new FileInputStream("Data\\statistic.xls");
-		HSSFWorkbook book = new HSSFWorkbook(cellData);
-		HSSFSheet sheet = book.createSheet("Sheet1");
-		HSSFRow row = sheet.createRow(1);
-		HSSFCell cell = row.createCell(2);
-		cell.setCellValue("test");
-		book.write(new FileOutputStream("Data\\statistic.xls"));
-		book.close();
+		//FileInputStream cellData = new FileInputStream("Data\\statistic.xls");
+		 HSSFWorkbook workbook = new HSSFWorkbook(); // 새 엑셀 생성
+	        HSSFSheet sheet = workbook.createSheet("시트명"); // 새 시트(Sheet) 생성
+	        HSSFRow row = sheet.createRow(0); // 엑셀의 행은 0번부터 시작
+	        HSSFCell cell = row.createCell(0); // 행의 셀은 0번부터 시작
+	        cell.setCellValue("테스트"); //생성한 셀에 데이터 삽입
+	        try {
+	            FileOutputStream fileoutputstream = new FileOutputStream("data\\set.xlsx");
+	            workbook.write(fileoutputstream);
+	            fileoutputstream.close();
+	            System.out.println("엑셀파일생성성공");
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            System.out.println("엑셀파일생성실패");
+	        }
+
+
 	}
 	
 	public static void randomAbility(Character c) {
