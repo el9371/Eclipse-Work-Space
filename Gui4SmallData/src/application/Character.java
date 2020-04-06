b23fd5416b26b38c5fb327009291041ab9816c54
Cyber <Cyber@172.40.73.96>
2020-04-06 23:54:45 +0900

    20.04.06
    day 4

---
+++ Gui4SmallData/src/application/Character.java
@@ -0,0 +1,82 @@
+package application;
+
+public class Character {
+	static private int battleSpeed = 1;
+	private String name;
+	private int maxHp, str, dex, luk, hp;
+	private int hpAbility, strAbility, dexAbility, lukAbility;
+	boolean isAlive;
+	
+	public Character(String name) {
+		this.hpAbility = 0; this.dexAbility = 0; this.strAbility = 0; this.lukAbility = 0;
+		this.name = name;
+	}
+	
+	public String getName() { return this.name;} 
+	public int getMaxHp() {
+		return maxHp;
+	}
+	public void plusMaxHp() {
+		this.hpAbility += 1;
+	}
+	public int getStr() {
+		return str;
+	}
+	public void plusStr() {
+		this.strAbility += 1;
+	}
+	public int getDex() {
+		return dex;
+	}
+	public void plusDex() {
+		this.dexAbility += 1;
+	}
+	public int getLuk() {
+		return luk;
+	}
+	public void plusLuk() {
+		this.lukAbility += 1;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	public boolean isAlive() {
+		if (hp <= 0) return false;
+		return true;
+	}
+	public void setAlive(boolean isAlive) {
+		this.isAlive = isAlive;
+	}
+	public void set4Battle() {
+		this.maxHp = 100 + this.hpAbility * 40;
+		this.str = 0 + this.strAbility * 5;
+		this.dex = (100 - this.dexAbility * 15) * battleSpeed;
+		this.luk = 0 + this.lukAbility * 15;
+		this.isAlive = true;
+		this.hp = this.maxHp;
+	}
+	
+	public boolean getDamage(int dmg) {
+		if (this.isAlive) {
+		this.hp = this.hp - dmg;
+		if (this.hp <= 0) {
+			this.hp = 0; this.isAlive = false;
+			System.out.println(this.name + "님이 쓰러지셨습니다."); 
+			}
+		else System.out.println(this.name + "이 " + dmg + "의 피해를 입어 " + this.hp + "가 남았습니다.");
+		return true;
+		} return false;
+	}
+	
+	public void printAbility() {
+		System.out.println(this.hpAbility + " | " + this.strAbility + " | "
+				+ this.dexAbility + " | " + this.lukAbility);
+	}
+	
+	public String returnAbility() {
+		return this.hpAbility +"." + this.strAbility + "." + this.dexAbility + "." + this.lukAbility;
+	}
+}
---
+++ Gui4SmallData/src/application/Exec.java
@@ -0,0 +1,81 @@
+package application;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
+import org.ini4j.Wini;
+
+
+public class Exec {
+	
+	public void exec() throws Exception {
+		
+		Character i = new Character("Alpha"), u = new Character("Beta");
+		Thread t1 = new BattlePase(i, u), t2 = new BattlePase(u, i);
+		randomAbility(i); randomAbility(u);
+		i.set4Battle(); u.set4Battle();
+		t1.start();
+		t2.start();
+		while(i.isAlive && u.isAlive) Thread.sleep(100);
+		System.out.println("전투종료");
+		if (i.isAlive) writeIni(i,u);
+		else writeIni(u,i);
+	}
+	
+	public void writeIni(Character winner, Character loser) throws IOException, Exception {
+		File iniData = new File("Data\\data.ini");
+		Ini rData = new Ini(iniData);
+		Wini wData = new Wini(iniData);
+			/* for setting 
+			String arrAbility[] = new String[100];
+			int numberOfAbility = 0;
+			boolean isHere;
+			for (int i = 0; i < 10000; i++) {
+				isHere = false;
+				Character c = new Character("test");
+				randomAbility(c);
+				System.out.println(c.returnAbility());
+				for (int j = 0; j < numberOfAbility; j++)
+					if (arrAbility[j].equals(c.returnAbility())) {isHere = true; break;} 
+				if (!isHere) {
+					arrAbility[numberOfAbility] = c.returnAbility();
+					wData.put(c.returnAbility(), "number", numberOfAbility++);
+				}
+				//wData.put(c.returnAbility(), "Games", 0);
+				//wData.put(c.returnAbility(), "Wins", 0);
+			}
+			wData.store();
+		 */
+		String wString = winner.returnAbility(), lString = loser.returnAbility();
+		wData.put("Base", "Games", Integer.parseInt(rData.get("Base", "Games")) + 1);
+		wData.put(wString, "Games",  Integer.parseInt(rData.get(wString, "Games")) + 1);
+		wData.put(wString, "Wins",  Integer.parseInt(rData.get(wString, "Wins")) + 1);
+		wData.put(lString, "Games",  Integer.parseInt(rData.get(lString, "Games")) + 1);
+		wData.store(); 
+	}
+	
+	
+	public static void randomAbility(Character c) {
+		for (int i = 0 ; i < 5 ; i++) {
+			int rnd = (int)(Math.random() * 4) + 1;
+			switch(rnd) {
+			case 1:
+				c.plusMaxHp();
+				break;
+			case 2:
+				c.plusStr();
+				break;
+			case 3:
+				c.plusDex();
+				break;
+			case 4:
+				c.plusLuk();
+			}
+		}
+	}
+}
