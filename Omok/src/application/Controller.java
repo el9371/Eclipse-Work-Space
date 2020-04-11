f988ca589be58df93ef0c7af4dd1d3e7f8c2c1e1
Cyber <Cyber@172.40.73.96>
2020-04-11 17:00:15 +0900

    20.04.11

--- Omok/src/application/Controller.java
+++ Omok/src/application/Controller.java
@@ -16,43 +16,104 @@ import util.*;
 
 public class Controller implements Initializable{
 	//-------------------------------------------
-	// PRIVATE FIELD
+	// FXML FIELD
 	//-------------------------------------------
-	private GlassRobot robot = new GlassRobotImpl();
 	@FXML private AnchorPane mainPage;
 	@FXML private ImageView background;
-	
-	
+	//-------------------------------------------
+	// FXML FIELD
+	//-------------------------------------------
+	private GlassRobot robot = new GlassRobotImpl();
+	private int board[][] = new int[19][19]; //x축 y축 순서
 
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		background.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				Point2D point = robot.getMouseLocation();
-		        double x = point.getX();
-		        double y = point.getY();
-		        System.out.println("y = " + y);
-		        System.out.println("x = " + x);
-		        try {
-		        FileInputStream imageFile = new FileInputStream("image\\black.png");
-		        Image black = new Image(imageFile);
-		        ImageView blackStone = new ImageView(black);
-		        System.out.println(background.getScene());
-		        } catch (FileNotFoundException e) {System.out.println("파일엄서");}
-			}
-		});
+		for (int i = 0; i < 19; i++)
+			for(int j = 0; j < 19; j++)
+				board[i][j] = 0;
+		background.setOnMousePressed(clickOnBoard);
 	}
 	
 	public void testButton() {
-		Point2D point = robot.getMouseLocation();
-        double x = point.getX();
-        double y = point.getY();
-        System.out.println("y = " + y);
-        System.out.println("x = " + x);
-        Point2D tmp = new Point2D(200.0, 300.0);
-		robot.mouseMove(tmp);
+		clearBoard();
 	}
 	
+	public void clearBoard() {
+		for (int i = 0; i < 19; i++)
+			for(int j = 0; j < 19; j++)
+				board[i][j] = 0;
+		mainPage.getChildren().remove(2, mainPage.getChildren().size());
+	}
+	
+	//-------------------------------------------
+	// Handler for click on omok board
+	//-------------------------------------------
+	EventHandler<MouseEvent> clickOnBoard = new EventHandler<MouseEvent>() {
+		@Override
+		public void handle(MouseEvent event) {
+			Point2D point = robot.getMouseLocation();
+			//It has index problems when window is moved. Find a way
+			int y = (int)point.getY() - 231, x = (int)point.getX() - 550;
+			int yDevided = y / 21, xDevided = x / 21, yRemains = y % 21, xRemains = x % 21;
+			if (yRemains <= 10) yRemains = 0;
+			else yRemains = 1;
+			if (xRemains <= 10) xRemains = 0;
+			else xRemains = 1;
+			int xIndex = xDevided + xRemains, yIndex = yDevided + yRemains;
+			/* for setting 
+			System.out.println("-----------------");
+			System.out.println(xDevided+"    "+xRemains+"    "+xIndex);
+			System.out.println(yDevided+"    "+yRemains+"    "+yIndex);
+			System.out.println("-----------------");
+			*/
+			if (board[xIndex][yIndex] != 0)
+				return;
+			
+	        try {
+	        FileInputStream imageFile = new FileInputStream("image\\black.png");
+	        Image black = new Image(imageFile, 15, 15, false, false);
+	        ImageView blackStone = new ImageView(black);
+	        mainPage.setTopAnchor(blackStone, (double)(yDevided + yRemains) * 21.0 + 2.0);
+	        mainPage.setLeftAnchor(blackStone, (double)(xDevided + xRemains) * 21.0 + 4.0);
+	        mainPage.getChildren().add(blackStone);
+	        //System.out.println(xIndex+"    "+yIndex);
+	        board[xIndex][yIndex] = 1;
+	        } catch (FileNotFoundException e) {System.out.println("파일엄서");}
+	        
+	        if (isFive(xIndex, yIndex))
+	        	System.out.println("오목완성");
+		}
+	};
+	//-------------------------------------------
+	// function for omok rules
+	//-------------------------------------------
+	public boolean isFive(int x, int y) {
+		int sum = 0;
+		//typically horizontal
+		for (int j = 0; j <5; j++) {
+			sum = 0;
+			for (int i = 0; i < 5; i++) 
+				sum = sum + board[x-4+j+i][y];
+		}
+		if (sum == 5) return true;
+		sum = 0;
+		//typically vertical
+		for (int i = 0; i < 5; i++)
+			sum = sum + board[x][y-2+i];
+		if (sum == 5) return true;
+		sum = 0;
+		//typically diagonal1 (top left to bottom right
+		for (int i = 0; i < 5; i++) 
+			sum = sum + board[x-2+i][y-2+i];
+		if (sum == 5) return true;
+		sum = 0;
+		//typically diagonal1 (top right to bottom left
+		for (int i = 0; i < 5; i++) 
+			sum = sum + board[x+2-i][y-2+i];
+		if (sum == 5) return true;
+		sum = 0;
+		
+		return false;
+	}
 }
